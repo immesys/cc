2af2ca98b24f59901774c2f1e42bec0f3f1a19eb
Michael Andersen <m.andersen@cs.berkeley.edu>
2015-02-05 20:42:19 -0800

    Add cube

---
+++ cube.lua
@@ -0,0 +1,13 @@
+
+
+A = dofile "lib/abspos.lua"
+function sqr(x)
+    A.push()
+    A.goto(x,0,0)
+    A.goto(x,x,0)
+    A.goto(0,x,0)
+    A.goto(0,0,0)
+    A.pop()
+end
+
+sqr(5)
---
+++ lib/abspos.lua
@@ -0,0 +1,80 @@
+
+local mod = {}
+
+mod.x = 0
+mod.y = 0
+mod.z = 0
+mod.stack = {}
+mod.goto = function(x, y, z)
+    --do z first
+    if z > mod.z then
+        for i=mod.z+1, z do
+            while not turtle.up() do
+                turtle.digUp()
+            end
+            mod.z = i
+        end
+    elseif z < mod.z then
+        for i=mod.z-1, z, -1 do
+            while not turtle.down() do
+                turtle.digDown()
+            end
+            mod.z = i
+        end
+    end
+
+    --then do x
+    if x > mod.x then
+        turtle.turnRight()
+        for i=mod.x + 1, x do
+            while not turtle.forward() do
+                turtle.dig()
+            end
+            mod.x = i
+        end
+        turtle.turnLeft()
+    elseif x < mod.x then
+        turtle.turnLeft()
+        for i=mod.x -1, x, -1 do
+            while not turtle.forward() do
+                turtle.dig()
+            end
+            mod.x = i
+        end
+        turtle.turnRight()
+    end
+
+    --then do y
+    if y > mod.y then
+        for i=mod.y + 1, y do
+            while not turtle.forward() do
+                turtle.dig()
+            end
+            mod.y = i
+        end
+    elseif y < mod.y then
+        turtle.turnLeft()
+        turtle.turnLeft()
+        for i=mod.y -1, y, -1 do
+            while not turtle.forward() do
+                turtle.dig()
+            end
+            mod.y = i
+        end
+        turtle.turnRight()
+        turtle.turnRight()
+    end
+end
+
+mod.push = function()
+    table.insert(mod.stack, {mod.x, mod.y, mod.z} )
+    mod.x = 0
+    mod.y = 0
+    mod.z = 0
+end
+
+mod.pop = function()
+   mod.x, mod.y, mod.z = unpack(table.remove(mod.stack))
+end
+
+return mod
