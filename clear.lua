bcaa876b5a07c2553b499ae145b20f1b7a62d961
Michael Andersen <michael@steelcode.com>
2015-02-06 19:27:55 -0800

    Create clear.lua

---
+++ clear.lua
@@ -0,0 +1,39 @@
+function vslice(z,w,h)
+  for i=1,z do
+    slice(w,h)
+    turtle.up()
+  end
+  for i=1,z do
+    turtle.down()
+  end
+end
+
+function slice(w, h)
+  for col=1,w do
+    fow row=1,h do
+      while not turtle.forward() do
+        turtle.dig()
+      end
+    end
+    if col%2 == 1 then
+      turtle.turnRight()
+    else
+      turtle.turnLeft()
+    end
+    while not turtle.forward() do
+      turtle.dig()
+    end
+    if col %2 == 1 then
+      turtle.turnRight()
+    else
+      turtle.turnLeft()
+    end
+  end
+  turtle.turnLeft()
+  for i=1,pw do
+    while not turtle.forward()
+    do turtle.dig() end
+  end
+  turtle.turnRight()
+end --fun
+vslice(pz,pw,ph)
